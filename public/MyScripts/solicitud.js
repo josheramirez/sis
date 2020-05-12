b2946d00996d212dfffb90ae41fe76a3827db1aa
Jose Luis Ramirez <jose.ramirez@ssmoc.gob.cl>
2020-05-12 10:00:49 -0400

    correccion busqueda rut con verificador incluido

--- public/MyScripts/solicitud.js
+++ public/MyScripts/solicitud.js
@@ -322,7 +322,38 @@ $(".madre").change(function (e) {
     }
 });
 
+var Fn = {
+	// Valida el rut con su cadena completa "XXXXXXXX-X"
+	validaRut : function (rutCompleto) {
+		rutCompleto = rutCompleto.replace("‐","-");
+		if (!/^[0-9]+[-|‐]{1}[0-9kK]{1}$/.test( rutCompleto ))
+			return false;
+		var tmp 	= rutCompleto.split('-');
+		var digv	= tmp[1]; 
+		var rut 	= tmp[0];
+		if ( digv == 'K' ) digv = 'k' ;
+		
+		return (Fn.dv(rut) == digv );
+	},
+	dv : function(T){
+		var M=0,S=1;
+		for(;T;T=Math.floor(T/10))
+			S=(S+T%10*(9-M++%6))%11;
+		return S?S-1:'k';
+	}
+}
+
+
+
 function consultar(d1) {
+    
+ console.log($("#run").val().trim());
+
+    if(!Fn.validaRut( $("#run").val().trim())){
+        toastr.warning('Ingresa un Rut correcto');
+        return;
+    }
+
     
     var rut_original = $('#run').val();
     rut = rut_original.split('-');
